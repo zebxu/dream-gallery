e49f1bafcfa06204ea9582b2258cb8e222b4e0fb
zeyangxu <zx638@nyu.edu>
2019-03-25 18:17:11 -0400

    disable webpack sourcemap

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express'),
-  mongoose = require('mongoose'),
   cors = require('cors'),
   path = require('path'),
   hbs = require('express-handlebars');
@@ -13,21 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
-// const db = require('./config/keys').mongoURI;
-
-// mongoose
-//   .connect(
-//     db,
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => {
-//     console.log('Mongo connected');
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-
-// const movies = require('./routes/api/movies');
 const pages = require('./routes/pages/movie_page');
 
 // app.use('/api/movies', movies);
